22d2edfb342ff8eaa584aad51cf724109f291be2
Jimmy DC <jimmydalecleveland@gmail.com>
2021-01-11 14:19:22 -0700

    link formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,22 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Gatsby Root Site</h1>
     <p>This is to test proxies with path prefixes in Gatsby sites</p>
-    <Link to="/page-2/">Go to page 2 (belongs to this site)</Link> <br />
-    <Link to="/blog/">Go to /blog root</Link>
-    <Link to="/blog/page-2">Go to /blog page 2</Link>
-    <Link to="/blog/page-2?kbid=7777">
-      Go to /blog page 2 with query params
-    </Link>
+
+    <h2>
+      <Link to="/page-2/">Go to page 2 (belongs to this site)</Link> <br />
+    </h2>
+
+    <h2>
+      <Link to="/blog/">Go to /blog root</Link>
+    </h2>
+    <h2>
+      <Link to="/blog/page-2">Go to /blog page 2</Link>
+    </h2>
+    <h2>
+      <Link to="/blog/page-2?kbid=7777">
+        Go to /blog page 2 with query params
+      </Link>
+    </h2>
   </Layout>
 )
 
