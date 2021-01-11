b2f6bd555dbc4956aef71ef1cae2bc56cbd214db
Jimmy DC <jimmydalecleveland@gmail.com>
2021-01-11 13:00:30 -0700

    add links to proxy locations

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,19 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h1>Gatsby Root Site</h1>
+    <p>This is to test proxies with path prefixes in Gatsby sites</p>
+    <Link to="/page-2/">Go to page 2 (belongs to this site)</Link> <br />
+    <Link to="/blog/">Go to /blog root</Link>
+    <Link to="/blog/page-2">Go to /blog page 2</Link>
+    <Link to="/blog/page-2?kbid=7777">
+      Go to /blog page 2 with query params
+    </Link>
   </Layout>
 )
 
